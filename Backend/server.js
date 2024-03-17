cd43b773f5c2acfaf36610bb49547134df3daf93
Charindu Liyanage <charinduliyanage17@gmail.com>
2024-03-18 00:55:16 +0530

    backend config

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+//configuring js
+const express  = require("express"); //declaring variables and importing packages here
+const mongoose = require("mongoose");
+const bodyParser=require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+
+const PORT = process.env.PORT || 8070; //local computer this server
+
+//body parser - because you have keys and their values in packege.json
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL; //access url
+
+mongoose.connect(URL, {
+    useCreateIndex : true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify : false,
+    
+  });
+//opening a function using arrows
+const connection = mongoose.connection;
+connection.once("open",()=> {
+    console.log("MongoDB database connected successfully!");
+});
+//run in the port
+app.listen(PORT , () => {
+    console.log(`Server is running on port ${PORT}`);
+});
